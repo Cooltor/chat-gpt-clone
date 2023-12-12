c9fab429f68d2bdf1a7e18e4a50c2d8bb4a47a5f
Cooltor <romaindbus86@gmail.com>
2023-12-12 11:34:25 +0100

    final

---
+++ server.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+const PORT = 8000;
+
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = process.env.API_KEY;
+
+app.post("/completions", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: req.body.message,
+        },
+      ],
+      max_tokens: 100,
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (err) {
+    console.err(err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,106 @@
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [value, setValue] = useState();
+  const [message, setMessage] = useState();
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState("");
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  };
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue("");
+  };
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({ message: value }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await fetch("http://localhost:8000/completions", options);
+      const data = await res.json();
+
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(previousChats);
+
+  useEffect(() => {
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        { title: currentTitle, role: "user", content: value },
+        { title: currentTitle, role: message.role, content: message.content },
+      ]);
+      setCurrentTitle(value);
+    }
+  }, [currentTitle, message]);
+
+  const currentChat = previousChats.filter(
+    (previousChat) => previousChat.title === currentTitle
+  );
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((previousChat) => previousChat.title))
+  );
+
+  console.log(uniqueTitles);
+
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div className="app">
+      <section className="side-bar">
+        <button onClick={createNewChat}>+ New chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => (
+            <li key={index} onClick={() => handleClick(uniqueTitle)}>
+              {uniqueTitle}
+            </li>
+          ))}
+        </ul>
+
+        <nav className="contacts">
+          <p> Made by Romain Dubus</p>
+        </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>Fifi GPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => (
+            <li key={index}>
+              <p className="role">{chatMessage.role}</p>
+              <p>{chatMessage.content}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <div id="submit" onClick={getMessages}>
+              ➡️
+            </div>
+          </div>
+          <p className="info">chat GPT 3.5 Version. Free research Preview</p>
+        </div>
+      </section>
     </div>
   );
 }
-
-export default App;
